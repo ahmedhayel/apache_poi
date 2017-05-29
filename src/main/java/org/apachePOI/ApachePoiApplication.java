6e6ced5e78b91c6d4b1a84672e120028107e5efd
ahmed <ahmed0hayel@gmail.com>
2017-05-29 14:42:14 +0000

    first-commit

---
+++ src/main/java/org/apachePOI/ApachePoiApplication.java
@@ -0,0 +1,12 @@
+package org.apachePOI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApachePoiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApachePoiApplication.class, args);
+	}
+}
